eb4e7044ef0142de5fee8778fd74d0c88f044513
Lucas Henrique <lucas.dpl@gmail.com>
2025-10-06 13:53:49 -0300

    feat:database

---
+++ lib/app/core/database/data_base.dart
@@ -0,0 +1,19 @@
+import 'package:ecommerce/app/core/database/dao/cart/cart_dao.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+Future<Database> getDatabase() async {
+  var databasesPath = await getDatabasesPath();
+  final String path = join(databasesPath, 'prosystem.db');
+
+  var db = await openDatabase(
+    path,
+    version: 1,
+    onCreate: (db, version) {
+      db.execute(CartDao.createTableCart);
+      db.execute(CartDao.createTableCartOrder);
+    },
+    onUpgrade: (db, oldVersion, newVersion) {},
+  );
+  return db;
+}
